fb5d5d26b945bf64e1bdffd49c31808f70eb1d90
Yaqoob <happyyaqoob@gmail.com>
2021-02-05 07:45:42 -0800

    Create Ninjas component and nested in root component

---
+++ veujs-playlist/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  <h1>{{title}}</h1>
+  <ninjas></ninjas>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Ninja App'
+    }
+  }
+}
+</script>
+
+
+<style> </style>
