2ac9b5226f273b4bea1a912eb5b9bcbac5479de5
Yaqoob <happyyaqoob@gmail.com>
2021-02-05 03:04:27 -0800

    Created two Vue instance and communicate each other

--- first-app/app.js
+++ first-app/app.js
@@ -1,22 +1,32 @@
-new Vue({
-    el:'#vue-app',
+var one = new Vue({
+    el:'#vue-app-one',
     data: {
-      health: 100,
-      ended: false
+      Title:'Vue App One'
     },
     methods: {
-      punch: function(){
-        this.health -=10;
-        if(this.health<= 0){
-          this.ended = true;
-        }
-      },
-      restart: function(){
-        this.health = 100;
-        this.ended = false;
+
+    },
+    computed:{
+      greet: function(){
+        return 'Hello from app one :)';
+      }
+    }
+});
+
+
+var two = new Vue({
+    el:'#vue-app-two',
+    data: {
+      Title:'Vue App Two'
+    },
+    methods: {
+      changeTitle: function(){
+        one.Title = 'Title Changed';
       }
     },
     computed:{
-
+      greet: function(){
+        return 'Yo dudes, this is app 2 speaking to ya:)';
+      }
     }
 });
