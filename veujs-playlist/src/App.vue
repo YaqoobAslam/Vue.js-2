7293839d22054a7f5e3affa2b5e4975619bf9a33
Yaqoob <happyyaqoob@gmail.com>
2021-02-05 23:08:36 -0800

    Using primitive type to change the title

--- veujs-playlist/src/App.vue
+++ veujs-playlist/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <app-header></app-header>
+    <app-header v-bind:title="title"></app-header>
     <app-content v-bind:ninjas="ninjas"></app-content>
-    <app-footer></app-footer>
+    <app-footer v-bind:title="title"></app-footer>
   </div>
 </template>
 
@@ -27,7 +27,8 @@ export default {
       {name: 'Tango', specialty: 'Conditionals', show: false},
       {name: 'Kami', specialty: 'Webpack', show: false},
       {name: 'Yoshi', specialty: 'Data Diggin', show: false}
-    ]
+    ],
+    title: "Vue App"
     }
   }
 }
--- veujs-playlist/src/components/Header.vue
+++ veujs-playlist/src/components/Header.vue
@@ -1,17 +1,26 @@
 <template>
 <header>
-  <h1>{{title}}</h1>
+  <h1 v-on:click="changeTitle">{{title}}</h1>
 </header>
 </template>
 
 
 <script>
 export default {
-
+  props: {
+    title:{
+    type:String
+    }
+  },
   data () {
     return {
       title: 'Vue App'
     }
+  },
+  methods:{
+  changeTitle: function(){
+    this.title= 'Vue Wizard'
+  }
   }
 }
 </script>
