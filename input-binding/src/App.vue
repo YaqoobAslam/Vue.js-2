e51235e1e6fd0027f45da86d38e1cf04e4b724b2
Yaqoob <happyyaqoob@gmail.com>
2021-02-07 02:16:54 -0800

    added new component and display data in preview section

---
+++ input-binding/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <add-blog></add-blog>
+  </div>
+</template>
+<script>
+import addBlog from './components/addBlog.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'add-blog': addBlog
+  }
+}
+</script>
+
+<style>
+  body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+  }
+</style>
