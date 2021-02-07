a6db849962603b7e506f51f84a3d762861160e6c
Yaqoob <happyyaqoob@gmail.com>
2021-02-06 21:34:38 -0800

    created form using slot to display data in formHelper component

--- slot-project/src/components/formHelper.vue
+++ slot-project/src/components/formHelper.vue
@@ -1,16 +1,41 @@
 <template>
   <div>
-    <slot name="title"></slot>
-    <h1>I am the form Helper</h1>
-    <slot name="text"></slot>
+    <h1>Please fill out form...</h1>
+    <form>
+      <div id="form-header">
+        <slot name="form-header"></slot>
+      </div>
+
+      <div id="form-fields">
+      <slot name="form-fields"></slot>
+      </div>
+
+      <div id="form-controls">
+      <slot name="form-controls"></slot>
+      </div>
+
+      <div id="useful-link">
+        <ul>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
+          <li><a href="#">Link 4</a></li>
+        </ul>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+
+  },
+  data(){
+
+  },
+  methods: {
+
   }
 }
 </script>
@@ -18,6 +43,27 @@ export default {
 
 <style scoped>
 h1{
-color: red;
+text-align: center;
+}
+form{
+width: 100%;
+max-width: 960px;
+margin: 0 auto;
+}
+#useful-links ul{
+padding: 0;
+}
+#useful-links li{
+display: inline-block;
+margin-right: 10px;
+}
+form > div{
+padding: 20px;
+background: #eee;
+margin: 20px 0;
+}
+#form-header{
+background: #ddd;
+border: 1px solid #bbb;
 }
 </style>
