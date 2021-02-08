e93d4bbd4f690a885858035d8eb3d7bb3a1c452d
Yaqoob <happyyaqoob@gmail.com>
2021-02-08 02:59:07 -0800

    Created Firebase blog-project and added posts in firebase database

--- input-binding/src/components/addBlog.vue
+++ input-binding/src/components/addBlog.vue
@@ -59,11 +59,8 @@ export default {
   },
   methods: {
     post: function(){
-      this.$http.post('https://jsonplaceholder.typicode.com/posts',{
-        title: this.blog.title,
-        body: this.blog.content,
-        userId: 1
-      }).then(function(data){
+      this.$http.post('https://blog-project-131c4-default-rtdb.firebaseio.com/posts.json',this.blog)
+      .then(function(data){
         console.log(data);
         this.submitted = true 
       });
