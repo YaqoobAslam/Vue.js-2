6e9d64a5090d97e829df15c9706f621193028ced
Yaqoob <happyyaqoob@gmail.com>
2021-02-22 12:24:56 -0800

    Created components passes props to the components and custom events  via emit to parent

---
+++ myproject/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <Navbar></Navbar>
+    <AllFriends :friends="friends"  @delete="deleteFriends"  ></AllFriends>
+    <OnlineFriends :friends="friends"   ></OnlineFriends>
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    AllFriends,
+    OnlineFriends
+  },
+  data () {
+    return {
+      title: 'My first Vue app, woo :)',
+       friends: [
+        {name: "Mario", online: true},
+        {name: "Luigi", online: false},
+        {name: "Toad", online: true},
+        {name: "Bowser", online: false},
+      ]
+    }
+  },
+  methods: {
+    deleteFriends(payload){
+    // console.log(payload);
+    this.friends = this.friends.filter(friend =>{
+      return friend.name !== payload.name
+    })
+
+    }
+  }
+}
+</script>
+
+<style>
+  h1 {
+    color: #444;
+    font-weight: normal;
+    text-align: center;
+  }
+</style>
